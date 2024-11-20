22bc34ea60fd79646d550a0199fcd876f94f4a15
Vsevolod Zholobov <vszholobov@gmail.com>
2024-11-20 16:09:26 +0300

    Enable ini plu

--- script/format_ansible_inventory.py
+++ script/format_ansible_inventory.py
@@ -17,6 +17,7 @@ def main():
 
     # Генерация конфигурационного файла
     template = f"""
+
 [inventory]
 enable_plugins=ini
 
@@ -26,11 +27,6 @@ ansible_user=terraform
 
 [web_servers]
 """ + "\n".join([f"{server['name']} ansible_host={server['ip']}" for server in servers])
-
-    # Сохранение в файл
-    output_file = "inventory.ini"
-    with open(output_file, "w") as f:
-        f.write(template.strip())
 
     print(template.strip() + "\n")
 
